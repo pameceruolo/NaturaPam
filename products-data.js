6376c105e3bf80080cb64c366e644a98537dee7b
Alan Romero <eldoblecero@gmail.com>
2026-02-05 17:51:58 -0300

    Se añaden más productos

--- products-data.js
+++ products-data.js
@@ -1,5 +1,82 @@
 const products = [
   {
+    title: "Crema Nutritiva Tododia Hojas de Limón y Guanábana 400 ml",
+    price: "$15.000",
+    desc: `Cuidado inteligente y aroma refrescante.
+            • Acción prebiótica: mantiene el equilibrio natural de la flora de la piel
+            • Nutrición personalizada: estimula la reposición de nutrientes según cada tipo de piel
+            • Nutrición profunda: piel saludable en todas las capas
+            • 94% de ingredientes naturales: mayor afinidad con tu piel
+            • Piel suave y protegida: fácil de esparcir y rápida absorción
+            • Fragancia que sorprende: revitalizante y fresca, con notas aromáticas y cremosas`,
+    images: ["TododiaLimonyGuanabana03.jpg", "TododiaLimonyGuanabana02.jpg", "TododiaLimonyGuanabana01.jpg"],
+    available: true
+  },
+  {
+    title: "Crema Hidratante Corporal Ekos Estoraque 250 ml",
+    price: "$10.000",
+    desc: `Cuida e Hidrata tu piel
+            .
+            Disfrutá de un ritual de bienestar con la línea Ekos Estoraque. Estimula 2 veces la producción de colágeno en la piel y brinda 72 horas de hidratación para complementar tu ritual de bienestar.
+            Ritual de cuidado Ekos Estoraque en 3 pasos:
+
+            1. Limpieza: Limpia sin resecar manteniendo el pH natural de la piel
+            2. Hidratación: Hidrata tu cuerpo, manos y labios.
+            3. Perfumación: Fragancia fresca que viene del bosque verde y húmedo, combina la frescura revitalizante verde aromático del estoraque con toques florales. Una fragancia con ingredientes de la biodiversidad amazónica extraído de la hierba del estoraque.
+            Hecho en Brasil. Vegano.`,
+    images: ["PulpaEstoraque01.jpg", "PulpaEstoraque02.jpg"],
+    available: true
+  },
+  {
+    title: "Aceite Corporal Ekos Pitanga Preta 100 ml",
+    price: "$5.000",
+    desc: `Brillo natural y sensación de frescura
+
+            Es un tratamiento trifásico que:
+
+            • combina aceites vegetales con agua
+            • ofreciendo una hidratación profunda y una fragancia envolvente
+            • su fórmula emoliente y ligera se activa al agitar el envase
+            • permitiendo una rápida absorción sin dejar sensación grasosa
+            • ideal para todo tipo de piel
+            • deja la piel suave, luminosa y delicadamente perfumada
+            • manteniendo su elasticidad
+            • con notas florales de pitanga negra
+            • libre de ingredientes nocivos`,
+    images: ["AceitePitangaPreta01.jpg", "AceitePitangaPreta02.jpg"],
+    available: true
+  },
+  {
+    title: "Tododia Mango Rosa y Agua de Coco Body Splash 200 ml",
+    price: "$10.000",
+    desc: `Perfumación ligera y refrescante hecha especialmente para el verano.
+            • Tododia busca inspiración en la combinación de notas sorprendentes, traducidas en fragancias irresistibles
+            • Sensación de frescura y ligereza que da el toque final al cuidado de cada día
+            • Hidratación ligera de la piel: mantiene la hidratación de la piel a lo largo del día
+            • Fragancia refrescante y alegre con notas frutales.
+            
+            Cómo usar:
+            Rociá el body splash en abundancia para revivir la sensación agradable del baño. aplicá en muñecas, cuello, escote, detrás de las orejas y donde más desees, excepto en el rostro.`,
+    images: ["TododiaMangoCocoBS01.jpg", "TododiaMangoCocoBS02.jpg", "TododiaMangoCocoBS03.jpg"],
+    available: true
+  },
+  {
+    title: "Pulpa Hidratante Corporal Ekos Castaña 400 ml",
+    price: "$18.000",
+    desc: `Revitalizá tu piel con el poder de la castaña
+
+            • con acción anti resequedad, deja tu piel restaurada y nutrida
+            • mayor concentración de aceite crudo de castaña
+            • revitaliza la piel
+            • mejora visiblemente la textura
+            • producto vegano: con ingredientes de origen natural y vegetal de la Amazonía
+            • fórmulas biocompatibles con la piel y altamente biodegradables
+            • sin parabenos, siliconas ni aceites minerales
+            *las imágenes son ilustrativas, este producto está en una posición cenital. el contenido de cada producto es el indicado en su descripción`,
+    images: ["PulpaEkosCastanha01.jpg", "PulpaEkosCastanha02.jpg", "PulpaEkosCastanha03.jpg", "PulpaEkosCastanha04.jpg"],
+    available: true
+  },
+  {
     title: "Caja Crema Hidratante",
     price: "$15.000",
     desc: `Crema hidratante de alta calidad para el cuidado de la piel.`,
@@ -7,10 +84,19 @@ const products = [
     available: true
   },
   {
-    title: "Crema Hidratante Manos Maracuyá",
-    price: "$7.000",
-    desc: `Crema hidratante para manos con fragancia de maracuyá tropical.`,
-    images: ["CremaHidratanteManosMaracuja01.jpeg"],
+    title: "Nectar Hidratante para Manos Maracuyá 40gr",
+    price: "$4.000",
+    desc: `Un regalo de la naturaleza para tus manos
+
+            • fragancia fresca y delicada
+            • antiestrés cutáneo
+            • calma y protege la piel
+            • hidratación intensa que forma una película protectora
+            • uñas y cutículas con aspecto saludable
+            • base vegetal biocompatible con la piel
+            • altamente biodegradable
+            • libre de ingredientes potencialmente perjudiciales para vos y para el medio ambiente`,
+    images: ["NectarMaracuja01.jpeg", "NectarMaracuja02.jpg", "NectarMaracuja03.jpg"],
     available: true
   },
   {
@@ -56,9 +142,15 @@ const products = [
   {
     title: "Kriska Perfume",
     price: "$30.000",
-    desc: `Perfume Kriska con aroma fresco y envolvente.`,
-    images: ["KriskaPerfume01.jpeg","KriskaPerfume02.jpeg"],
-    available: false
+    desc: `Dulzura y sofisticación para momentos especiales
+
+            • Fragancia dulce, cálida y femenina
+            • De larga duración
+            • Perfecta para quienes disfrutan de aromas con personalidad
+            • Notas orientales envolventes que crean un aura sofisticada
+            • Ideal para los días más frescos o para la noche`,
+    images: ["KriskaPerfume01.jpeg", "KriskaPerfume02.jpeg", "KriskaPerfume03.jpg"],
+    available: true
   },
   {
     title: "Protector Solar 50 Facial Hidratación",
